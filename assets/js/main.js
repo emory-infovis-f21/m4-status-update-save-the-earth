634a73d90181dd81d078c1abc70a505a7675c261
annamola <annamola12345@gmail.com>
2021-11-29 12:59:47 -0500

    size updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,138 +1,140 @@
 // ------- Scroll reveal animation -------
 const sr = ScrollReveal({
-    distance: '30px',
+    distance: "30px",
     duration: 1800,
     reset: true,
 });
 
-sr.reveal(`.home__data, .map_container,
+sr.reveal(
+    `.home__data, .map_container,
             .scatter_container,
-            .sunburst_container`, {
-    origin: 'top',
-    interval: 200,
-})
+            .sunburst_container`,
+    {
+        origin: "top",
+        interval: 200,
+    }
+);
 
 sr.reveal(`h2`, {
-    origin: 'left'
-})
+    origin: "left",
+});
 
 // sr.reveal(`.sunburst_container`, {
 //     origin: 'right'
 // })
 
-
 // ------- streamgraph js code -------
-  // var margin = {top: 10, right: 30, bottom: 30, left: 60}
-  // var width = 560 -margin.left - margin.right;
-  // var height= 500 - margin.top - margin.bottom
-  //
-  // var svg = d3.select("scatterplot")
-  //   .append("svg")
-  //     .attr("width", width + margin.left + margin.right)
-  //     .attr("height", height + margin.top + margin.bottom)
-  //   .append("g")
-  //     .attr("transform",
-  //       "translate(" + margin.left+ ","+ margin.top + ")");
-  //
-  // // d3.csv('historical_emissions.csv')
-  // //   .then(function(data){
-  // //     console.log(data);
-  // //     console.log(data.columns);
-  // // })
-  //
-  // // fake data to populate the visualization to produce the framework
-  // d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv", function(error, data){
-  //     console.log(data);
-  //
-  //     var keys = data.columns.slice(1)
-  //   // d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv")
-  //   // .then(function(data){
-  //   //   console.log(data);
-  //   //   console.log(data.columns);
-  //
-  //     //add x axis
-  //     var x = d3.scaleLinear()
-  //       .domain(d3.extent(data, function(d) {return d.year;}))
-  //       .range([0, width]);
-  //     svg.append("g")
-  //       .attr("transform", "translate(0, " + height*0.8 + ")")
-  //       .call(d3.axisBottom(x).tickSize(-height*.7).tickValues([1900, 1925, 1975, 2000]))
-  //       .select(".domain").remove()
-  //
-  //     svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
-  //
-  //     svg.append("text")
-  //       .attr("text-anchor", "end")
-  //       .attr("x", width)
-  //       .attr("y", height-30)
-  //       .text("Year")
-  //
-  //     var y = d3.scaleLinear()
-  //       .domain([-100000, 100000])
-  //       .range([height, 0]);
-  //
-  //     var color = d3.scaleOrdinal()
-  //       .domain(keys)
-  //       .range(d3.schemePaired);
-  //
-  //     //stack the data
-  //     var stackData = d3.stack()
-  //       .offset(d3.stackOffsetSilhouette)
-  //       .keys(keys)
-  //       (data)
-  //
-  //     //toop tip
-  //     var Tooltip = svg
-  //       .append("text")
-  //       .attr("x", 0)
-  //       .attr("y", 0)
-  //       .style("opacity", 0)
-  //       .style("font-size", 17)
-  //
-  //     //interactions
-  //     const mouseover = function(d){
-  //       Tooltip.style("opacity", 1)
-  //       d3.selectAll(".myArea").style("opacity", .2)
-  //       d3.select(this)
-  //         .style("stroke", "black")
-  //         .style("opacity", 1)
-  //     }
-  //
-  //     const mousemove = function(d, i){
-  //       grp = keys[i]
-  //       Tooltip.text(grp)
-  //     }
-  //
-  //     const mouseleave = function(d){
-  //       Tooltip.style("opacity", 0)
-  //       d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none")
-  //     }
-  //
-  //     // area generator
-  //     const area = d3.area()
-  //       .x(function(d){ return x(d.data.year); })
-  //       .y0(function(d){ return y(d[0]); })
-  //       .y1(function(d){ return y(d[1]); })
-  //
-  //       //show the area
-  //       svg.selectAll("mylayers")
-  //         .data(stackData)
-  //         .enter()
-  //         .append("path")
-  //           .attr("class", "myArea")
-  //           .style("fill", function(d){ return color(d.key); })
-  //           .attr("d", area)
-  //           .on("mouseover", mouseover)
-  //           .on("mousemove", mousemove)
-  //           .on("mouseleave", mouseleave)
-  //   })
-  //
-  // 
+// var margin = {top: 10, right: 30, bottom: 30, left: 60}
+// var width = 560 -margin.left - margin.right;
+// var height= 500 - margin.top - margin.bottom
+//
+// var svg = d3.select("scatterplot")
+//   .append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//     .attr("transform",
+//       "translate(" + margin.left+ ","+ margin.top + ")");
+//
+// // d3.csv('historical_emissions.csv')
+// //   .then(function(data){
+// //     console.log(data);
+// //     console.log(data.columns);
+// // })
+//
+// // fake data to populate the visualization to produce the framework
+// d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv", function(error, data){
+//     console.log(data);
+//
+//     var keys = data.columns.slice(1)
+//   // d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv")
+//   // .then(function(data){
+//   //   console.log(data);
+//   //   console.log(data.columns);
+//
+//     //add x axis
+//     var x = d3.scaleLinear()
+//       .domain(d3.extent(data, function(d) {return d.year;}))
+//       .range([0, width]);
+//     svg.append("g")
+//       .attr("transform", "translate(0, " + height*0.8 + ")")
+//       .call(d3.axisBottom(x).tickSize(-height*.7).tickValues([1900, 1925, 1975, 2000]))
+//       .select(".domain").remove()
+//
+//     svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
+//
+//     svg.append("text")
+//       .attr("text-anchor", "end")
+//       .attr("x", width)
+//       .attr("y", height-30)
+//       .text("Year")
+//
+//     var y = d3.scaleLinear()
+//       .domain([-100000, 100000])
+//       .range([height, 0]);
+//
+//     var color = d3.scaleOrdinal()
+//       .domain(keys)
+//       .range(d3.schemePaired);
+//
+//     //stack the data
+//     var stackData = d3.stack()
+//       .offset(d3.stackOffsetSilhouette)
+//       .keys(keys)
+//       (data)
+//
+//     //toop tip
+//     var Tooltip = svg
+//       .append("text")
+//       .attr("x", 0)
+//       .attr("y", 0)
+//       .style("opacity", 0)
+//       .style("font-size", 17)
+//
+//     //interactions
+//     const mouseover = function(d){
+//       Tooltip.style("opacity", 1)
+//       d3.selectAll(".myArea").style("opacity", .2)
+//       d3.select(this)
+//         .style("stroke", "black")
+//         .style("opacity", 1)
+//     }
+//
+//     const mousemove = function(d, i){
+//       grp = keys[i]
+//       Tooltip.text(grp)
+//     }
+//
+//     const mouseleave = function(d){
+//       Tooltip.style("opacity", 0)
+//       d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none")
+//     }
+//
+//     // area generator
+//     const area = d3.area()
+//       .x(function(d){ return x(d.data.year); })
+//       .y0(function(d){ return y(d[0]); })
+//       .y1(function(d){ return y(d[1]); })
+//
+//       //show the area
+//       svg.selectAll("mylayers")
+//         .data(stackData)
+//         .enter()
+//         .append("path")
+//           .attr("class", "myArea")
+//           .style("fill", function(d){ return color(d.key); })
+//           .attr("d", area)
+//           .on("mouseover", mouseover)
+//           .on("mousemove", mousemove)
+//           .on("mouseleave", mouseleave)
+//   })
+//
+//
 
 // ------- sunburst js code (it's really messy right now!!) -------
 var width = 275;
 var height = 275;
-var radius = Math.min(width, height) / 2;
+var radius = Math.min(width, height) / 2 + 40;
 var color = d3.scaleOrdinal(d3.schemeCategory20b);
 
 var c_select = "United States";
@@ -151,10 +153,7 @@ function updateChart(y_select, c_select) {
         .attr("width", width)
         .attr("height", height)
         .append("g")
-        .attr(
-            "transform",
-            "translate(" + width / 2 + "," + height / 2 + ")"
-        );
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     var partition = d3.partition().size([2 * Math.PI, radius]);
 
@@ -223,10 +222,10 @@ function updateChart(y_select, c_select) {
                 return d.x1;
             })
             .innerRadius(function (d) {
-                return d.y0;
+                return d.y0 - 40;
             })
             .outerRadius(function (d) {
-                return d.y1;
+                return d.y1 - 40;
             });
 
         g.selectAll("g")
@@ -239,15 +238,39 @@ function updateChart(y_select, c_select) {
                 return d.depth ? null : "none";
             })
             .attr("d", arc)
-            .on("mouseover", handleMouseOver)
-            .on("mouseover", handleMouseOver)
+            .on("mouseover", function (d) {
+                color_d = color((d.children ? d : d.parent).data.name);
+                if (d.depth == 2) {
+                    color_d = shadeColor(color_d, 20);
+                }
+                d3.select(this)
+                    .attr("r", 5.5)
+                    .style("fill", shadeColor(color_d, 10));
+            })
+            .on("mouseout", function (d) {
+                color_d = color((d.children ? d : d.parent).data.name);
+
+                if (d.depth == 2) {
+                    color_d = shadeColor(color_d, 20);
+                }
+                d3.select(this).attr("r", 5.5).style("fill", color_d);
+            })
             .style("stroke", "#fff")
             .style("fill", function (d) {
-                return color((d.children ? d : d.parent).data.name);
+                color_d = color((d.children ? d : d.parent).data.name);
+                console.log(d);
+                if (d.depth == 2) {
+                    return shadeColor(color_d, 20);
+                } else {
+                    return color_d;
+                }
             });
 
         g.selectAll(".node")
             .append("text")
+            .text(function (d) {
+                return d.parent ? d.data.name : "";
+            })
             .attr("transform", function (d) {
                 return (
                     "translate(" +
@@ -257,28 +280,32 @@ function updateChart(y_select, c_select) {
                     ")"
                 );
             })
-            .attr("dx", "-20")
+            .attr("dx", function (d) {
+                if (d.depth == 2) {
+                    return "-10";
+                } else {
+                    return "-25";
+                }
+            })
             .attr("dy", ".5em")
-            .style("font-size", "6px")
-            .text(function (d) {
-                return d.parent ? d.data.name : "";
+            .on("mouseover", function (d) {
+                color_d = color((d.children ? d : d.parent).data.name);
+                if (d.depth == 2) {
+                    color_d = shadeColor(color_d, 20);
+                }
+                d3.select(this.parentNode)
+                    .selectAll("path")
+                    .style("fill", shadeColor(color_d, 10));
+            })
+            .on("mouseout", function (d) {
+                d3.select(this).attr("r", 5.5).style("fill", "black");
+            })
+            .style("font-size", "6.3px")
+            .style("opacity", function (d) {
+                length = this.getComputedTextLength();
+                box = this.parentNode.getBBox();
+                return length > 50 || d.value < 150 ? 0 : 1;
             });
-
-        function handleMouseOver(d, i) {
-            console.log(d.data.name);
-            d3.select(this).attr({
-                fill: "orange",
-                r: radius * 2,
-            });
-        }
-        function handleMouseOut(d, i) {
-            console.log("mouseout");
-            d3.select(this).attr({
-                fill: "black",
-                r: radius,
-            });
-            d3.select("#t" + d.x + "-" + d.y + "-" + i).remove(); // Remove text location
-        }
     });
 }
 updateChart(y_select, c_select);
@@ -286,4 +313,23 @@ updateChart(y_select, c_select);
 function computeTextRotation(d) {
     var angle = ((d.x0 + d.x1) / Math.PI) * 90;
     return angle < 180 ? angle - 90 : angle + 90;
+}
+function shadeColor(color, percent) {
+    var R = parseInt(color.substring(1, 3), 16);
+    var G = parseInt(color.substring(3, 5), 16);
+    var B = parseInt(color.substring(5, 7), 16);
+
+    R = parseInt((R * (100 + percent)) / 100);
+    G = parseInt((G * (100 + percent)) / 100);
+    B = parseInt((B * (100 + percent)) / 100);
+
+    R = R < 255 ? R : 255;
+    G = G < 255 ? G : 255;
+    B = B < 255 ? B : 255;
+
+    var RR = R.toString(16).length == 1 ? "0" + R.toString(16) : R.toString(16);
+    var GG = G.toString(16).length == 1 ? "0" + G.toString(16) : G.toString(16);
+    var BB = B.toString(16).length == 1 ? "0" + B.toString(16) : B.toString(16);
+
+    return "#" + RR + GG + BB;
 }
