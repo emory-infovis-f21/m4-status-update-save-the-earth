536c6a4476be5880757dd9e5b72db30c1e193668
annamola <annamola12345@gmail.com>
2021-12-01 22:18:31 -0500

    add map

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,118 +19,6 @@ sr.reveal(`h2`, {
     origin: "left",
 });
 
-// sr.reveal(`.sunburst_container`, {
-//     origin: 'right'
-// })
-
-// ------- streamgraph js code -------
-// var margin = {top: 10, right: 30, bottom: 30, left: 60}
-// var width = 560 -margin.left - margin.right;
-// var height= 500 - margin.top - margin.bottom
-//
-// var svg = d3.select("scatterplot")
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//       "translate(" + margin.left+ ","+ margin.top + ")");
-//
-// // d3.csv('historical_emissions.csv')
-// //   .then(function(data){
-// //     console.log(data);
-// //     console.log(data.columns);
-// // })
-//
-// // fake data to populate the visualization to produce the framework
-// d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv", function(error, data){
-//     console.log(data);
-//
-//     var keys = data.columns.slice(1)
-//   // d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv")
-//   // .then(function(data){
-//   //   console.log(data);
-//   //   console.log(data.columns);
-//
-//     //add x axis
-//     var x = d3.scaleLinear()
-//       .domain(d3.extent(data, function(d) {return d.year;}))
-//       .range([0, width]);
-//     svg.append("g")
-//       .attr("transform", "translate(0, " + height*0.8 + ")")
-//       .call(d3.axisBottom(x).tickSize(-height*.7).tickValues([1900, 1925, 1975, 2000]))
-//       .select(".domain").remove()
-//
-//     svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
-//
-//     svg.append("text")
-//       .attr("text-anchor", "end")
-//       .attr("x", width)
-//       .attr("y", height-30)
-//       .text("Year")
-//
-//     var y = d3.scaleLinear()
-//       .domain([-100000, 100000])
-//       .range([height, 0]);
-//
-//     var color = d3.scaleOrdinal()
-//       .domain(keys)
-//       .range(d3.schemePaired);
-//
-//     //stack the data
-//     var stackData = d3.stack()
-//       .offset(d3.stackOffsetSilhouette)
-//       .keys(keys)
-//       (data)
-//
-//     //toop tip
-//     var Tooltip = svg
-//       .append("text")
-//       .attr("x", 0)
-//       .attr("y", 0)
-//       .style("opacity", 0)
-//       .style("font-size", 17)
-//
-//     //interactions
-//     const mouseover = function(d){
-//       Tooltip.style("opacity", 1)
-//       d3.selectAll(".myArea").style("opacity", .2)
-//       d3.select(this)
-//         .style("stroke", "black")
-//         .style("opacity", 1)
-//     }
-//
-//     const mousemove = function(d, i){
-//       grp = keys[i]
-//       Tooltip.text(grp)
-//     }
-//
-//     const mouseleave = function(d){
-//       Tooltip.style("opacity", 0)
-//       d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none")
-//     }
-//
-//     // area generator
-//     const area = d3.area()
-//       .x(function(d){ return x(d.data.year); })
-//       .y0(function(d){ return y(d[0]); })
-//       .y1(function(d){ return y(d[1]); })
-//
-//       //show the area
-//       svg.selectAll("mylayers")
-//         .data(stackData)
-//         .enter()
-//         .append("path")
-//           .attr("class", "myArea")
-//           .style("fill", function(d){ return color(d.key); })
-//           .attr("d", area)
-//           .on("mouseover", mouseover)
-//           .on("mousemove", mousemove)
-//           .on("mouseleave", mouseleave)
-//   })
-//
-//
-
 // ------- sunburst js code (it's really messy right now!!) -------
 let box = document.querySelector(".sunburst_card");
 var width = box.offsetWidth;
@@ -138,8 +26,8 @@ var height = box.offsetHeight - 30;
 var radius = Math.min(width, height) / 2 + 80;
 var color = d3.scaleOrdinal(d3.schemeCategory20b);
 
-var c_select = "United States";
-var y_select = 2010;
+var c_select = "China";
+var y_select = 1980;
 
 groupBy = function (xs, key) {
     return xs.reduce(function (rv, x) {
@@ -157,8 +45,9 @@ function updateChart(y_select, c_select) {
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     var partition = d3.partition().size([2 * Math.PI, radius]);
+    var dataCsv = d3.csv("historical_emissions.csv");
 
-    d3.csv("historical_emissions.csv", function (error, data) {
+    dataCsv.then(function (data) {
         json = data.map(function (value, key) {
             return {
                 Country: value.Country,
